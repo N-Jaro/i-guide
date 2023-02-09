92248fa150173a402ca1299cf3c9bb222232e159
Nattapon Jaroenchai <b_nj18@live.com>
2023-02-09 16:22:48 -0600

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-import 'flowbite';
+const importFlowbiteFunc = function(flowbitePathStr)
+{
+    const flowbiteScriptEl = document.createElement('script')
+    flowbiteScriptEl.setAttribute(
+        'src', flowbitePathStr
+    )
+    document.body.appendChild(flowbiteScriptEl)
+}
+importFlowbiteFunc('../node_modules/flowbite/dist/flowbite.js') // here goes your path to a local flowbite.js you want to import\
+
 const tabElements = [
     {
         id: 'profile',
@@ -25,20 +34,41 @@ const tabElements = [
 // options with default values
 const options = {
     defaultTabId: 'settings',
-    activeClasses: 'text-[#F4824A] hover:text-[#F4824A] border-[#F4824A]',
-    inactiveClasses: 'text-[#F4824A] hover:text-[#F4824A] border-[#F4824A]',
+    activeClasses: 'text-ig-orange hover:text-[#F4824A] border-[#F4824A]',
+    inactiveClasses: 'text-black hover:text-[#F4824A] border-[#F4824A]',
     onShow: () => {
         console.log('tab is shown');
     }
 };
 
-
-
-
-
 /*
 * tabElements: array of tab objects
 * options: optional
 */
+// import { Tabs } from 'flowbite';
 const tabs = new Tabs(tabElements, options);
 tabs.show('contacts');
+
+tailwind.config = {
+    theme: {
+        extend: {
+            textShadow: {
+                sm: '0 1px 2px var(--tw-shadow-color)',
+                DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+                lg: '0 8px 16px var(--tw-shadow-color)',
+            },
+        },
+    },
+    plugins: [
+        plugin(function ({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    'text-shadow': (value) => ({
+                        textShadow: value,
+                    }),
+                },
+                { values: theme('textShadow') }
+            )
+        }),
+    ],
+}
